4b932beb771b4d3ccf468c5c3e1bf5604314990e
Xyle Shield <155109492+Xyle0@users.noreply.github.com>
2024-11-30 03:46:37 +0200

    Add files via upload

---
+++ silver.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Windows.Forms;
+
+namespace authguard1
+{
+    public partial class Login : MetroFramework.Forms.MetroForm
+    {
+        public 8()
+        {
+            
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (Program.auth_sample.login(username.Text, password.Text))
+            {
+              
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        void OnFormClosing(object sender, FormClosingEventArgs e)
+        {
+           
+        }
+    }
+}
